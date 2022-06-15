bd2649a4fb18f3e7b535e40b632a51b8d41bfdc6
Weather-OS <tpapata1386@gmail.com>
2022-06-15 22:50:25 +0430

    Update some files

---
+++ Source/DiscordIntegration/Client/ClientObject.js
@@ -0,0 +1,35 @@
+/**
+ * Component name: Discord Client Object
+ * 
+ * Component description: Basic Client Object initalization.
+ * 
+ * Component exports: 
+ *      __comp__ > */ module.exports = __comp__ /*
+ */
+
+/**
+ * Imports:
+ *  __PLACEHOLDER__ Discord.js > */ const Discord = require("discord.js") /*
+ *  
+ */
+
+const __comp__ = {
+    OpenBotClient: class OpenBotClient {
+        constructor (
+
+        )
+        /**
+         * Main Client Object
+         */
+        {
+            
+        }
+
+        InitializeClient (
+            Preferences,
+            Plugins            
+        ) {
+            
+        }
+    }
+}
---
+++ Source/Start/Client/initClient.js
@@ -0,0 +1,20 @@
+/**
+ * Module name: Initialize Client.
+ * 
+ * Module Description: Initializes the client with the given arguments, private keys, public configurations and Plugins.
+ * 
+ * Module exports: 
+ *      __main__ > */ module.exports = __main__ /* 
+ *
+ * Global exports: non
+ * 
+ * TODO: 
+ * 
+ * Author: Weather
+ */
+
+const __main__ = {
+    InitializeBasicClient() {
+
+    }
+}
